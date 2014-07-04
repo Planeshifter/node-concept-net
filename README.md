5c75f0c9a95d73daf63add6aaabd9456a1d0642e
Planeshifter <pburckhardt@outlook.com>
2014-07-04 17:20:41 +0200

    ConceptNet API first commit

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-node-wiktionary
+node-ConceptNet
 ===============
 
-node.js interface to wiktionary.org RDF tags provided by DBpedia
+node.js interface to the ConceptNet semantic network API. For further information, consult the website of the project: 
+[http://conceptnet5.media.mit.edu/](http://conceptnet5.media.mit.edu/).
+
+# Introduction
+
+The ConceptNet package can be easily installed via npm:
+
+```
+npm install concept-net
+```
+
+To require the module in a project, we can use the expression
+
+```
+var cnet = require('concept-net');
+```
+
+# Getting Started 
+
+## ConceptNet
+
+### `.lookup(uri, [params], callback)`
+
+### `.search(params, callback)`
+
+### `.association(input, [params], callback)`
---
+++ src/store.js
@@ -0,0 +1,95 @@
+var http = require('http');
+var querystring = require('querystring');
+
+// constructor 
+function ConceptNet(){
+
+	if (!(this instanceof ConceptNet)){
+		return new ConceptNet();
+	}
+	
+	this.host = 'conceptnet5.media.mit.edu';
+	
+};
+
+
+ConceptNet.prototype.lookup = function(URI, params, callback){
+	
+	var limit = params.limit || 50;
+	var offset = params.offset || 0;
+	
+	var path = "/data/5.2" + String(URI) + "?limit=" + limit + "&offset=" + offset;
+	if (params.filter === "core") path += "&filter=core";
+	
+	var options = {};
+	options.host = this.host;
+	options.path = path;
+	
+	this.makeHtppRequest(options, callback);
+};
+
+ConceptNet.prototype.search = function(params, callback){
+	 var path = "/data/5.2/search?";
+	 
+	 path += querystring.stringify(params);
+	 
+	 var options = {};
+	 options.host = this.host;
+	 options.path = path;
+	 
+	 this.makeHtppRequest(options, callback);
+}
+
+function isConceptNetURI(uri){
+	var  myRegEx = /\/(?:[acdelrs]|and|or)\/[a-zA-Z]{2}\/\w+/;
+	return (myRegEx.test(uri) ? true : false);
+} 
+
+function isValidTermPath(path){
+	var myRegEx = /\/list\/[a-zA-Z]{2}\/\w+(?:@[-+]?[0-9]*\.?[0-9]+)?(,\w+(?:@[-+]?[0-9]*\.?[0-9]+)?)*/;
+	return (myRegEx.test(path) ? true : false);
+}
+
+ConceptNet.prototype.association = function(input, params, callback){
+		
+		if(!isConceptNetURI(input) && !isValidTermPath(input)){
+			var err = new Error("The input argument must be either a valid ConceptNet URI or a path of the form " +
+					"/list/<language>/<term list>");
+			return callback(err);
+		}
+	
+	 	 var path = "/data/5.2/assoc" + String(input) + "?limit" + limit;
+		 var limit = params.limit || 10;
+		 
+		 if (params.filter){
+			 if (isConceptNetURI(params.filter)) path += "&filter" + params.filter;	 
+			 else{
+				 var err = new Error("The GET argument filter must be a valid ConceptNet URI.");
+				 return callback(err);
+			 }
+		 }
+		 
+		 var options = {};
+		 options.host = this.host;
+		 options.path = path;
+		 
+		 this.makeHtppRequest(options, callback);
+};
+
+ConceptNet.prototype.makeHtppRequest = function(options, callback){
+	 var retrieve = function(response){
+	 var str = '';
+	 response.on('data', function (chunk) {
+	    str += chunk;
+	  });
+
+	  response.on('end', function () {
+	    callback(undefined, JSON.parse(str));
+	  });
+	 };
+	 
+	 http.request(options, retrieve).end();
+};
+
+
+module.exports = ConceptNet;
