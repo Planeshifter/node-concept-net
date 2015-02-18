b38894a86a5a6bd47f602f1f82d69f4beec0a180
Tony Issakov <tony@thefrontiergroup.com.au>
2015-02-18 20:33:41 +0800

    Added better support for non default host allowing  port and api version changing. Added tests. Fixed original code where host not passing through to constructor.

--- test/test.js
+++ test/test.js
@@ -1,27 +1,71 @@
 var assert = require("assert");
-var conceptNet = require('../src/store.js');	
+var conceptNet = require('../src/store.js');
 
 describe('conceptNet', function () {
 	  describe('config', function () {
-		  
-		  var cnet;
 		  it('creates an instance of conceptNet', function (done) {
-			  cnet = new conceptNet();
+			  var cnet = new conceptNet();
 			  assert(cnet instanceof conceptNet);
 			  done();
-		      });  
-		  
+		      });
+			describe('defaults', function(){
+				it('has default hostname', function (done) {
+					var cnet = new conceptNet();
+					assert(cnet.host == 'conceptnet5.media.mit.edu');
+					done();
+				});
+
+				it('has default port', function (done) {
+					var cnet = new conceptNet();
+					assert(cnet.port == 80);
+					done();
+				});
+
+				it('has default version', function (done) {
+					var cnet = new conceptNet();
+					assert(cnet.version = '5.2');
+					done();
+				});
+			});
+
+			describe('override', function(){
+				it('has set hostname', function (done) {
+					var cnet = new conceptNet('10.0.0.1','1234','5.3');
+					assert(cnet.host == '10.0.0.1');
+					done();
+				});
+
+				it('has set port', function (done) {
+					var cnet = new conceptNet('10.0.0.1','1234','5.3');
+					assert(cnet.port == 1234);
+					done();
+				});
+
+				it('has set version', function (done) {
+					var cnet = new conceptNet('10.0.0.1','1234','5.3');
+					assert(cnet.version == '5.3');
+					done();
+				});
+
+				it('only overrides the version', function(done){
+					var cnet = new conceptNet(null, null,'5.3');
+					assert(cnet.host == 'conceptnet5.media.mit.edu')
+					assert(cnet.version == '5.3');
+					done();
+				})
+			});
+
 		  it('looks up a single concept URI', function (done) {
-			  this.timeout(500);
-			  cnet = new conceptNet();
+			  this.timeout(1000);
+			  var cnet = new conceptNet();
 			  cnet.lookup("/c/en/toast",{
 				    limit: 1,
 				    offset: 0,
 				    filter: "core"}, function(err, result){
 				      assert(result.numFound > 0)
 				      done()
-				    }) 
+				    }
+				)
 		  });
-		     
 	  });
 });
